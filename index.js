c643504e56ae6dc9eca46baffb3666c585f870de
wigzen <61306958+wigzen@users.noreply.github.com>
2022-10-18 10:26:49 +0530

    FIX: background and js link

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let tobeTranslate = document.getElementById("t1");
 let translated = document.getElementById("t2");
-let submit =document.getElementById('submit');
-const URL ='https://api.funtranslations.com/translate/minion.json?text='
-submit.addEventListener('click',getTranslated);
-function getTranslated(){
-    fetch(URL+tobeTranslate.value)
-    .then(response => response.json())
-    .then(data => { translated.textContent= data.contents.translated});
+let submit = document.getElementById("submit");
+const URL = "https://api.funtranslations.com/translate/minion.json?text=";
+submit.addEventListener("click", getTranslated);
+function getTranslated() {
+  fetch(URL + tobeTranslate.value)
+    .then((response) => response.json())
+    .then((data) => {
+      translated.textContent = data.contents.translated;
+    });
 }
